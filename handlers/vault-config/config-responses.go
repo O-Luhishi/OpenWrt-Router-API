031bd9f123335a2ca2dece197bcc8ddda2d91047
Oluhishi <oeluhishi@gmail.com>
2020-06-22 00:23:15 +0100

    Removed un-needed structs

--- handlers/vault-config/config-responses.go
+++ handlers/vault-config/config-responses.go
@@ -4,21 +4,6 @@ import (
 	"net/http"
 )
 
-type JsonResponse struct {
-	// Reserved field to add some meta information to the API response
-	Meta interface{} `json:"meta"`
-	Data interface{} `json:"data"`
-}
-
-type JsonErrorResponse struct {
-	Error *ApiError `json:"error"`
-}
-
-type ApiError struct {
-	Status int    `json:"status"`
-	Title  string `json:"title"`
-}
-
 func UbusResponse(s func() ([]byte, error), w http.ResponseWriter) {
 	stat, err := s()
 	w.Header().Set("Content-Type", "application/json")
